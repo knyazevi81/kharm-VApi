c8346b26258636ddbbd76e05b8ac51a66e8d0e95
khasan <khasanknyazev81@gmail.com>
2024-10-26 19:29:50 +0300

    Update router.py create new routers

--- src/users/router.py
+++ src/users/router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+import json
 
 from src.users.schemas import IntegAuth
 from src.config import settings
@@ -14,3 +15,62 @@ router = APIRouter(
 async def get_all_users(data: IntegAuth):
     if data.integration_token == settings.integration_token:
         return UserReq.get_list_users()
+    
+    return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Брат, ты не авторизован"
+    )
+
+@router.post("")
+async def get_user(user_id: str, data: IntegAuth):
+    if data.integration_token == settings.integration_token:
+        response = UserReq.get_user_info(user_id)
+        return response
+    
+    return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Брат, ты не авторизован"
+    )
+
+@router.post("/online_users")
+async def get_online_users(data: IntegAuth):
+    if data.integration_token == settings.integration_token:
+        response = UserReq.onlines()
+        return response
+    
+    return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Брат, ты не авторизован"
+    )
+
+
+@router.post("/get_connection")
+async def get_all_users(user_id: str, connection_id: int, data: IntegAuth):
+    if data.integration_token == settings.integration_token:
+        response = UserReq.get_user_info(user_id)
+        
+        if len(response["clientStats"]) < connection_id-1 or connection_id < 0:
+            return HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Брат, неверно указан айдишник подключения"
+            )
+
+        client_settings = json.loads(response["settings"])
+        client_stream_settings = json.loads(response["streamSettings"])
+
+        connection_key = [
+            f"{response['protocol']}://",
+            f"{client_settings['clients'][connection_id]['id']}@",
+            f"{settings.host}:{response['port']}?",
+            f"type={client_stream_settings['network']}&",
+            f"security={client_stream_settings['security']}#",
+            f"{response['remark']}-{response['clientStats'][connection_id]['email']}"
+        ]
+
+        return "".join(connection_key)
+    
+    return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Брат, ты не авторизован"
+    )
+
